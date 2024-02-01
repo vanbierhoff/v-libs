ebddfe2d131e2e3d7896f1af3b73af39c0785586
hoff <ie@fv.ee>
2024-02-01 21:04:09 +0300

    added base forms core

---
+++ apps/v-libs/src/app/modules/forms/forms.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FCoreModule } from '../../../../../../v/f-core/src/lib/form-mobule/f-core.module';
+import { BaseForm } from './models/base-form/base-form';
+import { createForm } from '../../../../../../v/f-core/src/lib/form-builder-functions/create-form';
+
+
+
+@Component({
+  selector: 'v-libs-forms',
+  standalone: true,
+  imports: [CommonModule, FCoreModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.css'
+})
+export class FormsComponent implements OnInit {
+
+
+  ngOnInit() {
+    const form = new BaseForm();
+    console.log(form);
+    const formItem = createForm<BaseForm>(BaseForm);
+    console.log(formItem.getField('baseInput'));
+  }
+}
