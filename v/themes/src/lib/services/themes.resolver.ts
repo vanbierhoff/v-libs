77132fd540b459d608b53eb8c8db5c63fedf276f
hoff <ie@fv.ee>
2024-11-28 10:15:41 +0300

    add descriptions for themes

--- v/themes/src/lib/services/themes.resolver.ts
+++ v/themes/src/lib/services/themes.resolver.ts
@@ -5,6 +5,7 @@ import { ThemeDataService } from './theme-data.service';
 
 /**
  * Resolve applies blocks and style before change to a route
+ * @description Usage if you need preload styles or css after go to route
  */
 export const ThemesResolver: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => Promise<void> = async (
   route: ActivatedRouteSnapshot,
@@ -17,7 +18,13 @@ export const ThemesResolver: (route: ActivatedRouteSnapshot, state: RouterStateS
   await themeDataService.loadThemes(route?.data?.['appliesStyleNames']);
 };
 
-
+/**
+ *
+ * @param appliesStyleNames uploaded name themes
+ * @param dataService - theme data service
+ * Function for styles preload  can be used in guards,services or APP_INITIALIZER tokens or etc
+ * @description Usage if you need preload styles or css after go to route
+ */
 export const themePreload = async (appliesStyleNames: Array<string>, dataService?: ThemeDataService) => {
   const themeDataService = dataService || inject(ThemeDataService);
   await themeDataService.loadThemes(appliesStyleNames);
