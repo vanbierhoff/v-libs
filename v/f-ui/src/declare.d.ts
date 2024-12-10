975c098518ce3fabff8d541126f85cba6730d72c
hoff <ie@fv.ee>
2024-12-10 16:02:40 +0300

    update lib

--- v/f-ui/src/declare.d.ts
+++ v/f-ui/src/declare.d.ts
@@ -9,3 +9,10 @@ declare module "*.css" {
   const content: string;
   export default content;
 }
+
+declare module "*.module.css" {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.css';
--- v/f-ui/src/index.ts
+++ v/f-ui/src/index.ts
@@ -1,7 +1,7 @@
 export * from './lib/ui-elements';
 
 // theme
-export * from './lib/base-theme/base-f-ui.theme';
+//export * from './lib/base-theme/base-f-ui.theme';
 export * from './lib/const';
 // for DI
 export * from './lib/as-token/component.token';
